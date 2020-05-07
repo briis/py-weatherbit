0fc1a6944fbc224c120fbd7b09175a364047cb19
Bjarne Riis <bjarne@briis.com>
2020-05-07 21:56:50 +0200

    Update test_client.py

--- test_client.py
+++ test_client.py
@@ -11,7 +11,7 @@ from weatherbitpypi.errors import WeatherbitError
 _LOGGER = logging.getLogger(__name__)
 
 
-API_KEY = "2efc1231d69c41099e7f8e8356414d68"
+API_KEY = "YOUR-API-KEY"
 LATITUDE = 55.625053
 LONGITUDE = 12.136619
 LANGUAGE = "da"
