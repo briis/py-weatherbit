2db41627e7c28e7f4725379453e1ff2838bed57f
“Bjarne <bjarne@briis.com>
2020-05-28 16:46:56 +0200

    Bumped to 0.9

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'weatherbitpypi',
   packages = ['weatherbitpypi'],
-  version = '0.8',
+  version = '0.9',
   license='MIT',
   description = 'Python Wrapper for Weatherbit API', 
   author = 'Bjarne Riis',
