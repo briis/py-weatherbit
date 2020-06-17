1dcf415f710bca5ee3d6603104d8aa0b21cdc71a
“Bjarne <bjarne@briis.com>
2020-06-17 07:29:10 +0200

    Bumped version to 0.18

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'weatherbitpypi',
   packages = ['weatherbitpypi'],
-  version = '0.17',
+  version = '0.18',
   license='MIT',
   description = 'Python Wrapper for Weatherbit API', 
   author = 'Bjarne Riis',
