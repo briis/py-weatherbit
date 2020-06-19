cd26bdfe9b5f16948c04f9e2e0d4bc4b067fde1e
“Bjarne <bjarne@briis.com>
2020-06-19 14:24:46 +0200

    Bumped v0.23

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     "": ["*.md"],
     "weatherbitpypi": ["translations/*.json"],
   },
-  version = '0.22',
+  version = '0.23',
   license='MIT',
   description = 'Python Wrapper for Weatherbit API', 
   author = 'Bjarne Riis',
