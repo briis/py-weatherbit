10ba7ff7c2e8fa1665c24bc0c91785890d8d17d5
“Bjarne <bjarne@briis.com>
2020-06-25 06:33:00 +0200

    Bumped to 0.24.2 - Polish translation added

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     "": ["*.md"],
     "weatherbitpypi": ["translations/*.json"],
   },
-  version = '0.24.1',
+  version = '0.24.2',
   license='MIT',
   description = 'Python Wrapper for Weatherbit API', 
   author = 'Bjarne Riis',
